78e8666c147179f495dc58fd1e68a41b6407dafb
Only1Aure <jonathon.edwards@student.uagc.edu>
2025-07-10 15:15:25 -0500

    fix: update API function format for Azure Static Web Apps

--- api/hello.js
+++ api/hello.js
@@ -1,3 +1,9 @@
-module.exports = async (context, req) => {
-  context.res.json({ message: "Hello from Azure Functions!" });
+module.exports = async function (context, req) {
+  context.res = {
+    // status: 200, /* Defaults to 200 */
+    body: { message: "Hello from Azure Functions!" },
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  };
 };
